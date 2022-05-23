386de5e79c0c98c5a17facaacaa5c69979244325
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-05-23 21:49:22 +0600

    delete

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-// function verifyJWT(req, res, next) {
-//   const authHeader = req.headers.authorization;
-//   console.log(authHeader);
-// if (!authHeader) {
-//   return res.status(401).send({ message: "unAuthorized Access" });
-// }
-// const token = authHeader.split(" ")[1];
-// jwt.verify(token, process.env.ADD_TOKEN, function (err, decoded) {
-//   if (err) {
-//     return res.status(403).send({ message: "Forbidden access" });
-//   }
-//   req.decoded = decoded;
-// });
-//   next();
-// }
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).send({ message: "unAuthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ADD_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
@@ -39,6 +39,7 @@ async function run() {
     const orderCollection = client.db("carFactory").collection("order");
     const reviewCollection = client.db("carFactory").collection("review");
     const usersCollection = client.db("carFactory").collection("users");
+    const productsCollection = client.db("carFactory").collection("users");
     const verifyAdmin = async (req, res, next) => {
       const requestUser = req.decoded.email;
 
@@ -127,18 +128,47 @@ async function run() {
       res.send({ result, token });
     });
     // Admin
-    app.put("/users/admin/:email", async (req, res) => {
+    app.put("/users/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
-      console.log(email);
-      const filter = { email: email };
-      const updateDoc = {
-        $set: { role: "admin" },
-      };
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.send(result);
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        console.log(email);
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
     });
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+
+      const isAdmin = user.role === "admin";
+      console.log(isAdmin);
+
+      res.send({ admin: isAdmin });
+    });
+    // Products
+    app.post("/service", async (req, res) => {
+      const my = req.body;
+      const result = await serviceCollection.insertOne(my);
+      res.send(result);
+    });
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
